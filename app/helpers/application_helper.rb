5d29cae0831285145c9564f19889407e35b64960
Ian <ipancham@gmail.com>
2010-10-07 01:35:44 -0400

    added hoozin.in logo and text phase I

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   
   def logo
-    logo = image_tag("logo.png", :alt => "Sample App", :class => "round") 
+    logo = image_tag("hooz_in_main_logo.png", :alt => "hooz.in", :class => "round") 
   end
   
   # Return a title on a per-page basis
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "hooz.in"
     if @title.nil?
       base_title
     else
